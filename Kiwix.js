6d989933d2308daed1ca752b32a87b1d981f2ffd
lzybluee <lzybluee@gmail.com>
2026-06-03 11:22:58 +0800

    Works for all wiki

--- Kiwix.js
+++ Kiwix.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kiwix
 // @namespace    https://github.com/lzybluee/Wiki
-// @version      3.3
+// @version      3.6
 // @description  1. Redirect content url to viewer url. 2. Redirect 404 page to search url. 3. Press title text or 'w' key will link to online page.
 // @author       Lzy
 // @match        *://127.0.0.1:8080/*
@@ -16,21 +16,19 @@
     const url = window.location;
     let match = null;
 
-    if (url.href.includes('wikipedia_zh_all_maxi_')) {
-        if ((match = url.pathname.match(/^\/content\/(.*)$/)) && window.self === window.top) {
-            const redirect_url = url.origin + '/viewer#' + match[1];
-            console.log('Redirect to:', redirect_url);
-            url.replace(redirect_url);
-        } else if ((match = url.pathname.match(/^\/content\/(.*?)\/(.*)$/)) && document.title === 'Page not found' && !document.querySelector(`link[rel='canonical']`)) {
-            const search_url = url.origin + '/search?books.name=' + match[1] + "&pattern=" + match[2];
-            window.top.document.title = 'Search: ' + decodeURIComponent(match[2]);
-            console.log('Search url:', search_url);
-            url.replace(search_url);
-        } else if ((match = url.pathname.match(/^\/content\/(.*?)\/(.*)$/)) && window.self !== window.top) {
-            window.top.document.title = (decodeURIComponent(match[2]) + (url.hash ? ' - ' + decodeURIComponent(url.hash.slice(1)) : '')).replaceAll('_', ' ');
-        } else if (url.pathname === '/search' && window.self !== window.top) {
-            window.top.document.title = 'Search: ' + new URL(url.href).searchParams.get('pattern');
-        }
+    if (window.self === window.top && (match = url.pathname.match(/^\/content\/(.*)$/))) {
+        const redirect_url = url.origin + '/viewer#' + match[1];
+        console.log('Redirect to:', redirect_url);
+        url.replace(redirect_url);
+    } else if (window.parent === window.top && (match = url.pathname.match(/^\/content\/(.*?)\/(.*)$/)) && document.title === 'Page not found' && !document.querySelector(`link[rel='canonical']`)) {
+        const search_url = url.origin + '/search?books.name=' + match[1] + "&pattern=" + match[2];
+        window.top.document.title = 'Search: ' + decodeURIComponent(match[2]);
+        console.log('Search url:', search_url);
+        url.replace(search_url);
+    } else if (window.parent === window.top && (match = url.pathname.match(/^\/content\/(.*?)\/(.*)$/))) {
+        window.top.document.title = document.title + (url.hash ? (' - ' + decodeURIComponent(url.hash.slice(1)).replaceAll('_', ' ')) : '');
+    } else if (window.parent === window.top && url.pathname === '/search') {
+        window.top.document.title = 'Search: ' + new URL(url.href).searchParams.get('pattern');
     }
 
     function link2wiki() {
